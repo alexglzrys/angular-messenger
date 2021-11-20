596fbfd491839f51c77fd6974ae754b99dc65504
Alejandro González Reyes <alexglzrys@gmail.com>
2021-11-19 20:47:10 -0600

    estilos login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  operation: string = 'login'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeOperation(event: Event, value: string) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    this.operation = value;
   }
 
 }
